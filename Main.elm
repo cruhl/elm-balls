25215b8d849b1de7d574afcbd330b76121e8c2f6
cruhl <connerruhl@carfax.com>
2016-08-01 17:12:46 -0500

    Updated to use list of points

--- Main.elm
+++ Main.elm
@@ -13,14 +13,22 @@ main =
 
 
 type alias Model =
+    { points : List Point
+    }
+
+type alias Point =
     { x : Float
     , y : Float
     }
 
 model : Model
 model =
-    { x = 0
-    , y = 0
+    { points =
+        [ Point 0 0
+        , Point 10 10
+        , Point 20 20
+        , Point 30 30
+        ]
     }
 
 
@@ -31,19 +39,23 @@ update msg model =
     case msg of
 
         Move ->
-            { model
-            | x = model.x + 1
-            , y = model.y + 1
-            }
+            let
+                movePoint = (\{x, y} -> { x = x + 1, y = y + 1 })
+            in
+                { model | points = List.map movePoint model.points }
 
 
 view : Model -> Html Msg
 view model =
+    div []
+        [ div [] (List.map coord model.points)
+        , button [ onClick Move ]
+            [ text "Move!" ]
+        ]
+
+coord : Point -> Html msg
+coord point =
     let
-        coords = "(" ++ toString model.x ++ ", " ++ toString model.y ++ ")"
+        coords = "(" ++ toString point.x ++ ", " ++ toString point.y ++ ")"
     in
-        div []
-            [ text coords
-            , button [ onClick Move ]
-                [ text "Move!" ]
-            ]
+        text coords
