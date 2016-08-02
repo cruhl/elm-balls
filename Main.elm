82f38e5b8df969da3317da48fd3c7b696504e414
cruhl <connerruhl@carfax.com>
2016-08-02 14:29:10 -0500

    Added bouncing balls

--- Main.elm
+++ Main.elm
@@ -1,61 +1,147 @@
 import Html.App
-import Html.Events exposing (onClick)
 import Html exposing (Html, div, button, text)
+import Html.Attributes exposing (style)
+
+import Window
+import AnimationFrame
+import Time exposing (Time)
 
 
 main : Program Never
 main =
-    Html.App.beginnerProgram
-        { model = model
-        , view = view
+    Html.App.program
+        { init = init ! []
         , update = update
+        , subscriptions = subscriptions
+        , view = view
         }
 
 
 type alias Model =
-    { points : List Point
+    { particles : List Particle
+    , windowSize : Window.Size
     }
 
-type alias Point =
-    { x : Float
+type alias Particle =
+    { r : Float
+    , x : Float
     , y : Float
+    , vx : Float
+    , vy : Float
     }
 
-model : Model
-model =
-    { points =
-        [ Point 0 0
-        , Point 10 10
-        , Point 20 20
-        , Point 30 30
+
+init : Model
+init =
+    { windowSize = Window.Size 500 500
+    , particles =
+        [ { r = 10, x = 10, y = 10, vx = 3, vy = 2 }
+        , { r = 25, x = 10, y = 10, vx = 4, vy = 2 }
         ]
     }
 
 
-type Msg = Move
+type Msg
+    = Tick Time
+    | Resize Window.Size
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
 
-        Move ->
-            let
-                movePoint = (\{x, y} -> { x = x + 1, y = y + 1 })
-            in
-                { model | points = List.map movePoint model.points }
+        Tick dt ->
+            physics dt model ! []
+
+        Resize size ->
+            { model | windowSize = size } ! []
+
+
+physics : Time -> Model -> Model
+physics dt model =
+    let
+        particles' = List.map
+            (\p -> p
+                |> gravity dt
+                |> move dt
+                |> bounce model.windowSize
+            )
+            model.particles
+    in
+        { model | particles = particles' }
+
+move : Time -> Particle -> Particle
+move dt p =
+    { p
+    | x = p.x + p.vx * dt
+    , y = p.y + p.vy * dt
+    }
+
+gravity : Time -> Particle -> Particle
+gravity dt p = { p | vy = p.vy - 0.05 }
+
+bounce : Window.Size -> Particle -> Particle
+bounce {width, height} p =
+    let
+        width' = toFloat width
+        height' = toFloat height
+
+        (x', vx') = applyBoundaries (p.x, p.vx) (0, width')
+        (y', vy') = applyBoundaries (p.y, p.vy) (0, height')
+    in
+        { p
+        | x = x'
+        , y = y'
+        , vx = vx'
+        , vy = vy'
+        }
+
+applyBoundaries : (Float, Float) -> (Float, Float) -> (Float, Float)
+applyBoundaries (pos, vel) (minBound, maxBound) =
+    if pos >= maxBound
+        then (maxBound, flipVel vel)
+
+    else if pos <= 0
+        then (0, flipVel vel)
+
+    else
+        (pos, vel)
+
+flipVel : Float -> Float
+flipVel = (*) -0.6
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model = Sub.batch
+    [ AnimationFrame.diffs Tick
+    , Window.resizes Resize
+    ]
 
 
 view : Model -> Html Msg
-view model =
-    div []
-        [ div [] (List.map coord model.points)
-        , button [ onClick Move ]
-            [ text "Move!" ]
-        ]
+view model = div [] (List.map renderParticle model.particles)
 
-coord : Point -> Html msg
-coord point =
+(=>) : a -> b -> (a, b)
+(=>) = (,)
+
+renderParticle : Particle -> Html msg
+renderParticle {x, y, r} =
     let
-        coords = "(" ++ toString point.x ++ ", " ++ toString point.y ++ ")"
+        addUnits = (\x -> x ++ "px")
+
+        left = toString (x - r) |> addUnits
+        bottom = toString (y - r) |> addUnits
+        size = toString (r * 2) |> addUnits
     in
-        text coords
+        div
+            [ style
+                [ "position" => "fixed"
+                , "left" => left
+                , "bottom" => bottom
+                , "width" => size
+                , "height" => size
+                , "border-radius" => "50%"
+                , "background" => "black"
+                ]
+            ]
+
+            []
