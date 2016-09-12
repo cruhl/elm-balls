1b32fb1eb8064967f953294a78c947e9cde1aa30
cruhl <connerruhl@carfax.com>
2016-09-12 11:13:41 -0500

    Used elm-format

--- Main.elm
+++ Main.elm
@@ -1,16 +1,17 @@
-import Html.App
-import Html exposing (Html, div, button, text)
-import Html.Attributes exposing (style)
+module Main exposing (..)
 
 import Window
 import AnimationFrame
 import Time exposing (Time)
+import Html.App exposing (program)
+import Html exposing (Html, div, button, text)
+import Html.Attributes exposing (style)
 
 
 main : Program Never
 main =
-    Html.App.program
-        { init = init ! []
+    program
+        { init = ( init, Cmd.none )
         , update = update
         , subscriptions = subscriptions
         , view = view
@@ -21,6 +22,7 @@ type alias Model =
     { particles : List Particle
     , windowSize : Window.Size
     }
+
 
 type alias Particle =
     { r : Float
@@ -45,10 +47,10 @@ type Msg
     = Tick Time
     | Resize Window.Size
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-
         Tick dt ->
             physics dt model ! []
 
@@ -59,78 +61,102 @@ update msg model =
 physics : Time -> Model -> Model
 physics dt model =
     let
-        particles' = List.map
-            (\p -> p
-                |> gravity dt
-                |> move dt
-                |> bounce model.windowSize
-            )
-            model.particles
+        particles' =
+            List.map
+                (\p ->
+                    p
+                        |> gravity dt
+                        |> move dt
+                        |> bounce model.windowSize
+                )
+                model.particles
     in
         { model | particles = particles' }
+
 
 move : Time -> Particle -> Particle
 move dt p =
     { p
-    | x = p.x + p.vx * dt
-    , y = p.y + p.vy * dt
+        | x = p.x + p.vx * dt
+        , y = p.y + p.vy * dt
     }
 
+
 gravity : Time -> Particle -> Particle
-gravity dt p = { p | vy = p.vy - 0.05 }
+gravity dt p =
+    { p | vy = p.vy - 0.05 }
+
 
 bounce : Window.Size -> Particle -> Particle
-bounce {width, height} p =
+bounce { width, height } p =
     let
-        width' = toFloat width
-        height' = toFloat height
+        width' =
+            toFloat width
 
-        (x', vx') = applyBoundaries (p.x, p.vx) (0, width')
-        (y', vy') = applyBoundaries (p.y, p.vy) (0, height')
+        height' =
+            toFloat height
+
+        ( x', vx' ) =
+            applyBoundaries ( p.x, p.vx ) ( 0, width' )
+
+        ( y', vy' ) =
+            applyBoundaries ( p.y, p.vy ) ( 0, height' )
     in
         { p
-        | x = x'
-        , y = y'
-        , vx = vx'
-        , vy = vy'
+            | x = x'
+            , y = y'
+            , vx = vx'
+            , vy = vy'
         }
 
-applyBoundaries : (Float, Float) -> (Float, Float) -> (Float, Float)
-applyBoundaries (pos, vel) (minBound, maxBound) =
-    if pos >= maxBound
-        then (maxBound, flipVel vel)
 
-    else if pos <= 0
-        then (0, flipVel vel)
-
+applyBoundaries : ( Float, Float ) -> ( Float, Float ) -> ( Float, Float )
+applyBoundaries ( pos, vel ) ( minBound, maxBound ) =
+    if pos >= maxBound then
+        ( maxBound, flipVelocity vel )
+    else if pos <= 0 then
+        ( 0, flipVelocity vel )
     else
-        (pos, vel)
+        ( pos, vel )
 
-flipVel : Float -> Float
-flipVel = (*) -0.6
+
+flipVelocity : Float -> Float
+flipVelocity =
+    (*) -0.6
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model = Sub.batch
-    [ AnimationFrame.diffs Tick
-    , Window.resizes Resize
-    ]
+subscriptions model =
+    Sub.batch
+        [ AnimationFrame.diffs Tick
+        , Window.resizes Resize
+        ]
 
 
 view : Model -> Html Msg
-view model = div [] (List.map renderParticle model.particles)
+view model =
+    div [] (List.map renderParticle model.particles)
 
-(=>) : a -> b -> (a, b)
-(=>) = (,)
+
+(=>) : a -> b -> ( a, b )
+(=>) =
+    (,)
+
 
 renderParticle : Particle -> Html msg
-renderParticle {x, y, r} =
+renderParticle { x, y, r } =
     let
-        addUnits = (\x -> x ++ "px")
+        addUnits =
+            (\x -> x ++ "px")
 
-        left = toString (x - r) |> addUnits
-        bottom = toString (y - r) |> addUnits
-        size = toString (r * 2) |> addUnits
+        left =
+            toString (x - r) |> addUnits
+
+        bottom =
+            toString (y - r) |> addUnits
+
+        size =
+            toString (r * 2) |> addUnits
     in
         div
             [ style
@@ -143,5 +169,4 @@ renderParticle {x, y, r} =
                 , "background" => "black"
                 ]
             ]
-
             []
